1d0dea22d0bdc612a4702c36c2352e2e1d5c2b8b
reiko_tsushima <reiko.qn8g.tsushima@misumi.co.jp>
2019-09-27 08:17:09 +0900

    スライド数量帯マージ作成：不要なカラムを削除 エラーフラグ作成済み

---
+++ 02.master_converter/FCN_Data_Check.py
@@ -0,0 +1,73 @@
+
+# ★★　商品マスターのみ　ヘッダが一致・Innerが一致するもののみを対象にしてください！！　★★
+import pandas as pd
+import numpy as np
+import csv
+import Type
+import Header
+import datetime
+print(datetime.datetime.now())
+
+# ★★　チェックするファイルを保存するフォルダを下記に指定してください！！　★★
+# ★★　必ず同じフォルダに保存してください！！ フォルダの「\」は「/」にしてください！！　★★
+path='//172.24.81.185/share1/share1c/加工品SBU/加工SBU共有/派遣/■Python_SPC_Master/Data_Check/'
+
+# ★★　基準となるtxtファイル名を変えてください！！　↓（緑色のここ）　★★
+a = pd.DataFrame(pd.read_csv(path + 'Product_Master_GRMプーリFCN_ECAL.txt',sep='\t', encoding='utf_16', dtype=str, engine='python', error_bad_lines=False))
+# ★★　チェック対象となるtxtファイル名を変えてください！！　↓（緑色のここ）　★★
+b = pd.DataFrame(pd.read_csv(path + 'FCN_to_ECAL_GRM03622710_Productmst_hou.txt',sep='\t', encoding='utf_16', dtype=str, engine='python', error_bad_lines=False))
+
+a = a.sort_values(by='Inner Code')
+b = b.sort_values(by='Inner Code')
+
+a = a.astype(Type.type())   # データ型をそろえる
+a = a.fillna('')
+a.to_csv(path + 'A_product.txt', sep='\t', encoding='utf_16', index=False)
+
+b = b.astype(Type.type())   # データ型をそろえる
+b = b.fillna('')
+b.to_csv(path + 'B_product.txt', sep='\t', encoding='utf_16', index=False)
+
+dictB ={}
+h = []
+with open(path + 'A_product.txt') as fileB:
+        reader = csv.reader(fileB, delimiter='\t')
+        reader = csv.reader((line.replace('\0','') for line in fileB))
+        for row in reader:
+            for sel in row:
+                dictB[sel] = True
+
+df = pd.DataFrame()
+df2 = pd.DataFrame()
+df3 = pd.DataFrame()
+with open(path + 'B_product.txt') as fileA:
+    reader = csv.reader(fileA, delimiter='\t')
+    reader = csv.reader((line.replace('\0', '') for line in fileA))
+    for row in reader:
+        for sel in row:
+            if sel not in dictB:
+                df = df.append(pd.Series(sel.split('\t')), ignore_index=True, sort=False)
+
+df = df[df[2] != 'XXX']
+if len(df) == 0:
+    print('Congratulations!')   # 不一致がない！！
+else:
+    df2 = df2.append(df[[3]], sort=False)
+    df2 = df2.rename(columns={3: 'Inner Code'})
+    a = pd.DataFrame(pd.read_csv(path + 'A_product.txt',sep='\t', encoding='utf_16', dtype=str, engine='python', error_bad_lines=False))
+    b = pd.DataFrame(pd.read_csv(path + 'B_product.txt',sep='\t', encoding='utf_16', dtype=str, engine='python', error_bad_lines=False))
+    a['data'] = 'A'
+    b['data'] = 'B'
+    a = pd.merge(df2, a, how='inner')
+    b = pd.merge(df2, b, how='inner')
+    df3 = df3.append(a, sort=False)
+    df3 = df3.append(b, sort=False)
+
+    df3 = df3.sort_values(by=['Inner Code','data'])
+
+    # ★★　出力するExcelファイル名を変えてください！！（緑色の部分）　★★
+    # ★★　不一致が多いとエラーの可能性あり　4G超え　★★
+    df3.to_excel(path + 'fcn_ecal_check.xlsx', index=False)
+
+print(datetime.datetime.now())
+print('fin')
