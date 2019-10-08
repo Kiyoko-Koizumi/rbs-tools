b8fb9618098b4f232e2c9a98066c029402571276
[MISUMI] Yuichi Kato <yuichi.fu98.kato@misumi.co.jp>
2019-10-08 15:37:59 +0900

    R.B.Sマスター作成処理を追加

--- 02.master_converter/script_file/menu.py
+++ 02.master_converter/script_file/menu.py
@@ -9,6 +9,7 @@ import os, tkinter, tkinter.filedialog, tkinter.messagebox
 import tkinter as tk
 import tkinter.ttk as ttk
 import fill_in_overlist_of_Unitprice
+import rbs_master
 
 csv.field_size_limit(1000000000)
 
@@ -20,12 +21,17 @@ def select_func():
 
     # ルートフレームの作成
     root = tk.Tk()
-    label1 = tk.Label(root, text="【処理内容を選択】", font=("", 12), height=2)
-    label2 = tk.Label(root, text="マスタ作成", font=("", 12), height=2)
-    label3 = tk.Label(root, text="R.B.Sマスタ作成", font=("", 12), height=2)
-    label4 = tk.Label(root, text="　　　　　", font=("", 12), height=2)
-    label5 = tk.Label(root, text="　　　　　", font=("", 12), height=2)
-    label6 = tk.Label(root, text="ミラー化調査", font=("", 12), height=2)
+    size = 9
+    font = u'メイリオ'
+    mes = "・商品別仕入先マスタ" + "\n" +"・RBSオーダー振替設定マスタ" + "\n" + "・RBS商品別仕入先マスタ"
+    # root.option_add('*font', u'メイリオ')
+    label1 = tk.Label(root, text="【処理内容を選択】", font=(font, size))
+    label2 = tk.Label(root, text="マスタ作成", font=(font, size))
+    label3 = tk.Label(root, text="R.B.Sマスタ作成", font=(font, size))
+    label4 = tk.Label(root, text="　　　　　", font=(font, size))
+    label5 = tk.Label(root, text="　　　　　", font=(font, size))
+    label6 = tk.Label(root, text="　　　　　", font=(font, size))
+    label7 = tk.Label(root, text=mes, font=(font, size), justify='left')
 
     # ボタンの作成（コールバックコマンドには、コンボボックスの値を取得する処理を定義）
     button1 = tk.Button(root, text="Product_master to ECAL",
@@ -46,12 +52,6 @@ def select_func():
                        command=lambda: fill_in_overlist_of_Unitprice.fill_in_overlist(), width=15)
     button6 = tk.Button(root, text="SPC 仕入値変更",
                        command=lambda: func1(), width=30)
-    button7 = tk.Button(root, text="商品別仕入先マスタ",
-                        command=lambda: func1(), width=30)
-    button8 = tk.Button(root, text="RBSオーダー振替設定マスタ",
-                       command=lambda: func1(), width=30)
-    button9 = tk.Button(root, text="RBS商品別仕入先マスタ",
-                       command=lambda: func1(), width=30)
     button10 = tk.Button(root, text="MCOST商品別仕入先数量スライドマスタ",
                        command=lambda: func1(), width=30)
     button11 = tk.Button(root, text="MCOST単価数量スライドマスタ",
@@ -59,7 +59,7 @@ def select_func():
     button12 = tk.Button(root, text="MCOST商品チェック詳細マスタ",
                        command=lambda: func1(), width=30)
     button14 = tk.Button(root, text="商品、商品別仕入先マスタより抽出",
-                       command=lambda: func1(), width=30)
+                       command=lambda: rbs_master.rbs_master(), width=30)
     button13 = tk.Button(root, text="処理終了",
                        command=lambda: sys.exit())
     # コンボボックスの配置
@@ -68,7 +68,7 @@ def select_func():
     label3.grid(row=8, column=0)
     label4.grid(row=0, column=1)
     label5.grid(row=0, column=3)
-    label6.grid(row=7, column=0)
+    label6.grid(row=7, column=3)
     # ボタンの配置
     button1.grid(row=1, column=2)
     button2.grid(row=2, column=2)
@@ -78,14 +78,12 @@ def select_func():
     button14.grid(row=3, column=4)
     button15.grid(row=4, column=4)
     button6.grid(row=6, column=2)
-    button14.grid(row=7, column=2)
-    button7.grid(row=8, column=2)
-    button8.grid(row=9, column=2)
-    button9.grid(row=10, column=2)
-    button10.grid(row=11, column=2)
-    button11.grid(row=12, column=2)
-    button12.grid(row=13, column=2)
-    button13.grid(row=14, column=5)
+    button14.grid(row=8, column=2)
+    label7.grid(row=9, column=2)
+    button10.grid(row=12, column=2)
+    button11.grid(row=13, column=2)
+    button12.grid(row=14, column=2)
+    button13.grid(row=15, column=5)
     root.mainloop()
     return
 
